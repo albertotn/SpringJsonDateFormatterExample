e6a5fb339760d9bf85649d9bfb7df9e24b7ecdbf
albertotn <12526457+albertotn@users.noreply.github.com>
2024-07-02 15:19:26 +0200

    added test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DemoApplicationTests {
 
+    @Autowired
+    TestController controller;
+    
+    TestModel model;
+  
+    @BeforeAll
+    void setup() {
+      model = controller.getTest();
+    }
+    
 	@Test
-	void contextLoads() {
+	void testNotNull() {
+	  assertThat(model!=null);
 	}
-
+	
+	@Test
+	void testValuesNotNull() {
+	  assertThat(model.getTimestamp()!=null);
+	  assertThat(model.getIsoTimestamp()!=null);
+	}
+	
 }
